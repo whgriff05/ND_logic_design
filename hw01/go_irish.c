977da387ec29a96ad15bb944f330532430a8c632
Will Griffin <willhgriffin@icloud.com>
2026-01-15 09:08:54 -0500

    Finished Part 2

--- hw01/go_irish.c
+++ hw01/go_irish.c
@@ -2,4 +2,28 @@
 
 int main() {
 	printf("Go Irish!\n");
+
+	// Part 2
+	int x = 2;
+	int y = 3;
+	printf("x = %d  y = %d  sum = %d\n", x, y, x+y);
+
+	x = 41;
+	printf("%d %x %c\n", x, x, x);
+
+	x = 0x74;
+	printf("%d %x %c\n", x, x, x);
+
+	x = '4';
+	printf("%d %x %c\n", x, x, x);
+
+	// Part 3
+
+	// Part 4
+
+	// Part 5
+
+	// Part 6
+
+	// Part 7
 }
