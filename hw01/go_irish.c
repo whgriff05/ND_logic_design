f5d3a724476e87b6778cdfeee9fcfbb653ff5e36
Will Griffin <willhgriffin@icloud.com>
2026-01-17 13:48:14 -0500

    Finished Part 3 and 4

--- hw01/go_irish.c
+++ hw01/go_irish.c
@@ -18,8 +18,12 @@ int main() {
 	printf("%d %x %c\n", x, x, x);
 
 	// Part 3
+    printf("%x %x %x\n", -1, -2, -3);
+    printf("%d %d %d %d\n", 0x7ffffffe, 0x7fffffff, 0x80000000, 0x80000001);
 
 	// Part 4
+    unsigned int p4 = 0xffffffff;
+    printf("%u\n", p4);
 
 	// Part 5
 
