9bc2a06f4d2edd1f6b68bb8705c17d51a3f02567
Will Griffin <willhgriffin@icloud.com>
2026-01-24 14:57:10 -0500

    Completed part 3

--- hw02/disassembler.c
+++ hw02/disassembler.c
@@ -1,15 +1,29 @@
 #include <stdio.h>
 
-void disassemble(int opcode);
+void disassemble(int hex_digit);
 
 int main(void) {
-    
+
+    // Test Cases
+    disassemble(0x1234);
     disassemble(0x0482);
     disassemble(0xA0F0);
     disassemble(0x53E0);
+
     return 0;
 }
 
-void disassemble(int opcode) {
+void disassemble(int hex_digit) {
+    int opcode = (hex_digit >> 12) & 0xF;
+    int rw = (hex_digit >> 8) & 0xF;
+    int ra = (hex_digit >> 4) & 0xF;
+    int rb = (hex_digit >> 0) & 0xF;
 
+    if ((1 << opcode) & 0xFF90) {
+        printf("Error: invalid opcode %x (0x%x)\n", opcode, hex_digit);
+        return;
+    }
+
+
+    printf("opcode = %x     rw = %x     ra = %x     rb = %x\n", opcode, rw, ra, rb);
 }
