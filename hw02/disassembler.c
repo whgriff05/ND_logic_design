1a04f277cf911949ef586c7a7039c59c854da388
Will Griffin <willhgriffin@icloud.com>
2026-01-24 14:16:09 -0500

    Started hw02

---
+++ hw02/disassembler.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void disassemble(int opcode);
+
+int main(void) {
+    
+    disassemble(0x0482);
+    disassemble(0xA0F0);
+    disassemble(0x53E0);
+    return 0;
+}
+
+void disassemble(int opcode) {
+
+}
